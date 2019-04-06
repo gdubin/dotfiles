363571487fa22a3dc93a1965b9fdcd0bb47d1cab
Garth Dubin <garth@codejunkie.org>
2019-04-05 20:01:22 -0400

    Adding gitprompt setup.

---
+++ .profile
@@ -0,0 +1,2 @@
+GIT_PROMPT_ONLY_IN_REPO=1
+source ~/dotfiles/plugins/.bash-git-prompt/gitprompt.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,29 @@
 ########
 # VIM!
 
+VIM_PLUGIN_DIR=~/.vim/pack/plugins/start/
+
 # Clean slate.
 rm -rf ~/.vim
 rm ~/.vimrc
 
 # Assuming vim 8, native package management.
-mkdir -p ~/.vim/pack/plugins/start/
-cd ~/.vim/pack/plugins/start/
-git clone https://github.com/scrooloose/nerdtree.git
-git clone git://github.com/vim-ruby/vim-ruby.git
+mkdir -p $VIM_PLUGIN_DIR
+git clone https://github.com/scrooloose/nerdtree.git $VIM_PLUGIN_DIR/nerdtree
+git clone git://github.com/vim-ruby/vim-ruby.git $VIM_PLUGIN_DIR/vim-ruby
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 ########
 # BASH!
 
+rm ~/.profile
 
+# Clean slate.
+rm -rf ~/dotfiles/plugins
+
+# Download all the things.
+mkdir ~/dotfiles/plugins
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/dotfiles/plugins/.bash-git-prompt --depth=1
+
+ln -s ~/dotfiles/.profile ~/.profile
